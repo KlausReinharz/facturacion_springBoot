0c872975cf31371a5baeddb9e66ad6bd8dec6f35
Alex Ponce <alexponce2753@gmail.com>
2024-10-22 10:09:16 -0500

    crud person

--- src/main/java/com/klaus/controller/PersonController.java
+++ src/main/java/com/klaus/controller/PersonController.java
@@ -1,15 +1,16 @@
 package com.klaus.controller;
 
 import com.klaus.entity.Person;
+import com.klaus.exceptions.BadRequestException;
 import com.klaus.services.admin.person.PersonRegistrationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/person")
@@ -21,6 +22,9 @@ public class PersonController {
     //listar Personas
     @GetMapping("/personas")
     public List<Person> getAllPerson(){
+       if(personRegistrationService.getListPerson()==null){
+           throw new BadRequestException("person list not found");
+       }
         return this.personRegistrationService.getListPerson();
     }
 
@@ -29,6 +33,32 @@ public class PersonController {
     public ResponseEntity<?> postPerson(@RequestBody Person person){
        Person nuevo =this.personRegistrationService.createPerson(person);
         return new ResponseEntity<>(nuevo, HttpStatus.CREATED);
+    }
+
+    //Obterner cliente por id
+    @GetMapping("/persons/{id}")
+    public ResponseEntity<?> getPersonById(@PathVariable Long id){
+        Optional<Person> person = this.personRegistrationService.getPersonId(id);
+        return person.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(()-> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    //editar
+    @PutMapping("/update")
+    public ResponseEntity<?>updatePerson(@RequestBody Person person){
+        this.personRegistrationService.updatePerson(person);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    //eliminar
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?>deletePerson(@PathVariable Long id){
+        if(this.personRegistrationService.getPersonId(id).isPresent()){
+            this.personRegistrationService.deletePerson(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 
